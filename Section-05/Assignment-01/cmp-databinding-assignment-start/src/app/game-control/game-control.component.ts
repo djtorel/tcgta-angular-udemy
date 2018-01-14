6fe4511645b73eac93067f629abad2eebaac1f46
Dominic Torruellas <dtorruellas@gmail.com>
2018-01-14 00:03:53 -0700

    Finished assignment for section 5

---
+++ Section-05/Assignment-01/cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { setInterval } from 'timers';
+
+@Component({
+  selector: 'app-game-control',
+  templateUrl: './game-control.component.html',
+  styleUrls: ['./game-control.component.css']
+})
+export class GameControlComponent implements OnInit {
+
+  @Output() intervalTicked = new EventEmitter<number>();
+  intervalStarted = false;
+  interval;
+  intervalNumber = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  startInterval() {
+    this.interval = setInterval(() => {
+      this.intervalTicked.emit(this.intervalNumber + 1);
+      this.intervalNumber++;
+    }, 1000);
+    this.intervalStarted = true;
+  }
+
+  stopInterval() {
+    if (this.interval) {
+      clearInterval(this.interval._id);
+    }
+    this.intervalStarted = false;
+  }
+
+}
