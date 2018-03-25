2cbd703a604d58ed7272a74ded009080d9783940
Dominic Torruellas <dtorruellas@gmail.com>
2018-03-25 12:51:19 -0700

    section 09 work done

---
+++ Section-09/services/src/app/account/account.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { AccountsService } from '../services/accounts.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+  constructor(private accountService: AccountsService) {
+  }
+
+  onSetTo(status: string) {
+    this.accountService.updateStatus(this.id, status);
+    this.accountService.statusUpdated.emit(status);
+  }
+}
---
+++ Section-09/services/src/app/new-account/new-account.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AccountsService } from '../services/accounts.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent {
+
+  constructor(private accountService: AccountsService) {
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(accountName, accountStatus);
+  }
+}
